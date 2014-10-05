853ecfee2721121088e1db1595a5cab095adcfd7
Valerii Hiora <valerii.hiora@gmail.com>
2014-10-05 13:57:51 +0300

    Further tuning up-doc script

--- up_doc.sh
+++ up_doc.sh
@@ -6,7 +6,8 @@ set -e
 echo '<meta http-equiv=refresh content=0;url=lmdb/index.html>' > target/doc/index.html
 sudo pip install ghp-import
 ghp-import -n target/doc
-git push -q -f https://${TOKEN}@github.com/${TRAVIS_REPO_SLUG}.git gh-pages
+git push -q -f https://${TOKEN}@github.com/${TRAVIS_REPO_SLUG}.git gh-pages > /dev/null 2>&1
+echo 'Pushed to gh-pages succesfully'
 rm target/doc/index.html
 mv target/doc .
 curl http://www.rust-ci.org/artifacts/put?t=$RUSTCI_TOKEN | sh
