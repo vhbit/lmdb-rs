b8b3e9bd02a504dd41bfa5db7061182313dc2a48
Valerii Hiora <valerii.hiora@gmail.com>
2014-11-20 17:00:53 +0200

    DbHandle to allow caching

--- src/lib.rs
+++ src/lib.rs
@@ -15,7 +15,8 @@ extern crate "liblmdb-sys" as ffi;
 
 #[stable]
 pub use ffi::{mdb_filehandle_t, MDB_stat, MDB_envinfo};
-pub use core::{EnvBuilder, Environment, EnvFlags, EnvCreateFlags, Database, DbFlags};
+pub use core::{EnvBuilder, Environment, EnvFlags, EnvCreateFlags};
+pub use core::{Database, DbFlags, DbHandle};
 pub use core::{Transaction, ReadonlyTransaction, MdbError, MdbValue};
 pub use core::{Cursor, CursorValue, CursorIter, CursorKeyRangeIter};
 pub use traits::{FromMdbValue, ToMdbValue};
