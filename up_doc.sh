0afd3e5c8bd719c61a715b6aeb98f279627c32bd
Valerii Hiora <valerii.hiora@gmail.com>
2015-01-12 11:14:04 +0200

    Travis: -> to docker-baked

--- up_doc.sh
+++ up_doc.sh
@@ -4,8 +4,8 @@ set -e
 [ $TRAVIS_BRANCH = master ]
 [ $TRAVIS_PULL_REQUEST = false ]
 echo '<meta http-equiv=refresh content=0;url=lmdb/index.html>' > target/doc/index.html
-sudo pip install ghp-import
-ghp-import -n target/doc
+pip install ghp-import --user $USER
+$HOME/.local/bin/ghp-import -n target/doc
 git push -q -f https://${TOKEN}@github.com/${TRAVIS_REPO_SLUG}.git gh-pages > /dev/null 2>&1
 echo 'Pushed to gh-pages succesfully'
 rm target/doc/index.html
