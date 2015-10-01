141a69892627d4617b6c95d26e28a46860251626
Valerii Hiora <valerii.hiora@gmail.com>
2015-10-01 17:33:57 +0300

    Silencing CircleCI warnings for gh-pages branch

--- up_doc.sh
+++ up_doc.sh
@@ -5,7 +5,7 @@ set -e
 [ $TRAVIS_PULL_REQUEST = false ]
 echo '<meta http-equiv=refresh content=0;url=lmdb_rs/index.html>' > target/doc/index.html
 pip install ghp-import --user $USER
-$HOME/.local/bin/ghp-import -n target/doc
+$HOME/.local/bin/ghp-import -n target/doc -m "Updated documentation [ci skip]"
 git push -q -f https://${TOKEN}@github.com/${TRAVIS_REPO_SLUG}.git gh-pages > /dev/null 2>&1
 echo 'Pushed to gh-pages succesfully'
 rm target/doc/index.html
