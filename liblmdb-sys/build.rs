70e1d00953e42d2ab30c9f87a8ef25e697986dac
Paul Lietar <paul@lietar.net>
2016-07-08 08:41:10 -0700

    Don’t use pkg-config to find liblmdb.
    liblmdb doesn’t seem to distribute pkgconfig support files.

--- liblmdb-sys/build.rs
+++ liblmdb-sys/build.rs
@@ -1,19 +1,16 @@
-extern crate pkg_config;
 extern crate gcc;
 
 fn main() {
     let target = std::env::var("TARGET").unwrap();
 
-    if !pkg_config::find_library("liblmdb").is_ok() {
-        let mut config = gcc::Config::new();
-        config.file("mdb/libraries/liblmdb/mdb.c")
-              .file("mdb/libraries/liblmdb/midl.c");
+    let mut config = gcc::Config::new();
+    config.file("mdb/libraries/liblmdb/mdb.c")
+          .file("mdb/libraries/liblmdb/midl.c");
 
-        if target.contains("dragonfly") {
-            config.flag("-DMDB_DSYNC=O_SYNC");
-            config.flag("-DMDB_FDATASYNC=fsync");
-        }
-
-        config.compile("liblmdb.a");
+    if target.contains("dragonfly") {
+        config.flag("-DMDB_DSYNC=O_SYNC");
+        config.flag("-DMDB_FDATASYNC=fsync");
     }
+
+    config.compile("liblmdb.a");
 }
