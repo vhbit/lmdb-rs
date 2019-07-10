721e4c0cb4ba899e44f1f8e951e794d23991cfb8
Ivan Sorokin <ivan@sorokin.io>
2019-07-10 22:19:37 +0200

    Update lmdb dependency

--- liblmdb-sys/build.rs
+++ liblmdb-sys/build.rs
@@ -1,11 +1,12 @@
-extern crate gcc;
+extern crate cc;
 
 fn main() {
     let target = std::env::var("TARGET").unwrap();
 
-    let mut config = gcc::Config::new();
-    config.file("mdb/libraries/liblmdb/mdb.c")
-          .file("mdb/libraries/liblmdb/midl.c");
+    let mut config = cc::Build::new();
+    config
+        .file("mdb/libraries/liblmdb/mdb.c")
+        .file("mdb/libraries/liblmdb/midl.c");
     config.opt_level(2);
 
     if target.contains("dragonfly") {
