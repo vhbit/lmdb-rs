54c9c0fe4e9371e8fb76593023c72150858f376f
Ivan Sorokin <ivan@sorokin.io>
2020-01-12 17:21:15 +0100

    Fix iOS build
    Android and iOS version can not be built from the same version of LMDB.
    From some X commits ago LMBD build for iOS would fail on
    mdb_env_open(...). Also, Android build is only possible after Y commits
    ago, where Y > X. So, the only solution is to build from different
    versions for each arch.
    
    `mdb_ios` folder is a dump of `https://github.com/LMDB/lmdb` repo on
    ad8488cfac644d7a289e428ab3c403c859d844cb commit.

--- liblmdb-sys/build.rs
+++ liblmdb-sys/build.rs
@@ -4,9 +4,15 @@ fn main() {
     let target = std::env::var("TARGET").unwrap();
 
     let mut config = cc::Build::new();
-    config
-        .file("mdb/libraries/liblmdb/mdb.c")
-        .file("mdb/libraries/liblmdb/midl.c");
+    if target.contains("ios") {
+        config
+            .file("mdb_ios/libraries/liblmdb/mdb.c")
+            .file("mdb_ios/libraries/liblmdb/midl.c");
+    } else {
+        config
+            .file("mdb/libraries/liblmdb/mdb.c")
+            .file("mdb/libraries/liblmdb/midl.c");
+    }
     config.opt_level(2);
 
     if target.contains("dragonfly") {
