d15114248413c7db5ef8b410e7f040b9c633e52b
Muneer <elendilm@gmail.com>
2021-06-01 21:39:23 +0530

    refactor(liblmdb-sys): use cc crate instead of gcc

--- liblmdb-sys/build.rs
+++ liblmdb-sys/build.rs
@@ -1,9 +1,9 @@
-extern crate gcc;
+extern crate cc;
 
 fn main() {
     let target = std::env::var("TARGET").unwrap();
 
-    let mut config = gcc::Config::new();
+    let mut config = cc::Build::new();
     config.file("mdb/libraries/liblmdb/mdb.c")
           .file("mdb/libraries/liblmdb/midl.c");
     config.opt_level(2);
